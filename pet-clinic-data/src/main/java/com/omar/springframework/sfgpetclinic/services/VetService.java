413cfe0026f6b1059a69a3cdce02558544e12eec
Omar Morales Ortega <morales.ortega.omar@gmail.com>
2020-06-19 18:18:33 -0500

    Adding services interfaces. Closes #4.

---
+++ pet-clinic-data/src/main/java/com/omar/springframework/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.omar.springframework.sfgpetclinic.services;
+
+import com.omar.springframework.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
