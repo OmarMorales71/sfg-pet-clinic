d80d8d02d0704f549db4460715a0c29ffc7b146d
Omar Morales <morales.ortega.omar@gmail.com>
2020-07-17 21:42:42 -0500

    Added SpecialityService and its SpecialityMap

---
+++ pet-clinic-data/src/main/java/com/omar/springframework/sfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.omar.springframework.sfgpetclinic.services;
+
+import com.omar.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long>{
+}
