41b247be78d93ecacebb6a1dfd684f2cef8ea644
Omar Morales <morales.ortega.omar@gmail.com>
2020-08-26 13:52:42 -0500

    Added Visit Controller and Mappings. Closes #57

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/PetController.java
@@ -65,7 +65,9 @@ public class PetController {
             model.addAttribute("pet", pet);
             return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
         }else{
+            pet.setOwner(owner);
             petService.save(pet);
+
             return "redirect:/owners/"+owner.getId();
         }
     }
